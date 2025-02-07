7549e6e4adb9b3fe6ec9c1330a427e981a605949
Esteban Mosquera <teebaanmosquera@gmail.com>
2025-02-06 23:01:55 -0500

    Juego completado

--- app.js
+++ app.js
@@ -2,3 +2,40 @@
 
 let amigos = [];
 
+function agregarAmigo(){
+    const amigo = document.getElementById('amigo').value;
+    if (amigo){
+        amigos.push(amigo);
+        document.getElementById('amigo').value='';
+        mostrarAmigos();
+    }else{
+        alert('Ingresa un nombre por favor');
+    }
+}
+
+function mostrarAmigos(){
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    amigos.forEach(amigo => {
+        const nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = amigo;
+        lista.appendChild(nuevoElemento);
+    });
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("No hay amigos disponibles para el sorteo");
+        return;        
+    }
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    amigos.splice(indiceAleatorio, 1);
+    mostrarAmigos()
+    document.getElementById('resultado').innerHTML = `El amigo sorteado es: <strong>${amigoSorteado}</strong>`;
+
+}
+
+
+agregarAmigo();
+sortearAmigo();
